367b0b59554aa568dfcca1c1d174552e27ef7794
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-08 21:23:05 -0300

    apostila02: listagem 2.2.12 concluida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OlaAngularComponent } from './ola-angular/ola-angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OlaAngularComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'ola-angular',
+  templateUrl: './ola-angular.component.html',
+  styleUrls: ['./ola-angular.component.css'] 
+})
+export class OlaAngularComponent{
+
+}
